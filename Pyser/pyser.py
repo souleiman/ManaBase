0929480cf098718ddcb0384ecf0184760eda6648
Souleiman <souleiman2010@gmail.com>
2014-08-19 21:45:21 -0400

    updated code

--- Pyser/pyser.py
+++ Pyser/pyser.py
@@ -1,30 +1,29 @@
+"""
+Usage: pyser -u <url>
+
+-u, --url   The resource URL
+"""
 __author__ = 'Souleiman'
 
-import sys
-import urllib.request
-import json
 import sqlite3
 import re
 
+import requests
+from docopt import docopt
 
 def setup_card(card):
     c_type = ' '.join([str(x) for x in card['types']])
 
     for opt in ['manaCost', 'text', 'flavor']:
         if opt in card:
-            card[opt] = re.sub('[\{\}]', '', card[opt])
-            card[opt] = re.sub('[\n]', ' ', card[opt])
+            card[opt] = re.sub(r'[{}]', '', card[opt])
+            card[opt] = card[opt].replace('\n', ' ')
 
-    db_card = dict()
-    db_card['id'] = card['multiverseid']
-    db_card['ascii'] = card['name']
-    db_card['name'] = card['name']
-    db_card['power'] = card['power'] if 'power' in card else None
-    db_card['toughness'] = card['toughness'] if 'toughness' in card else None
-    db_card['mana'] = card['manaCost'] if 'manaCost' in card else None
-    db_card['loyalty'] = card['loyalty'] if 'loyalty' in card else None
-    db_card['link'] = None
-    db_card['ability'] = None
+    db_card = {'id': card['multiverseid'], 'ascii': card['name'], 'name': card['name'],
+               'power': card['power'] if 'power' in card else None,
+               'toughness': card['toughness'] if 'toughness' in card else None,
+               'mana': card['manaCost'] if 'manaCost' in card else None,
+               'loyalty': card['loyalty'] if 'loyalty' in card else None, 'link': None, 'ability': None}
 
     if 'Creature' in c_type:
         db_card['type'] = "{main_type} \u2014 {archetype}" \
@@ -49,46 +48,46 @@ def setup_card(card):
         db_card['type'] = '{main_type}{0}'.format(
             " \u2014 {0}".format(' '.join(card['subtypes'])) if 'subtypes' in card else '',
             main_type=c_type)
-
         db_card['ability'] = card['text']
     else:
         raise Exception("Unknown type: {0}".format(c_type))
-
     return db_card
 
 
-def process(url):
-    request = urllib.request.urlopen(url)
-    json_string = request.read()
-    obj = json.loads(json_string.decode('utf-8'))
-
-    cards = obj['cards']
+def process_manabase_resource(url):
+    response = requests.get(url)
+    cardset = response.json()
     db_conn = sqlite3.connect('../manabase.db')
     db_cur = db_conn.cursor()
 
+    cards = cardset['cards']
     added = 0
     skipped = 0
-    for i in cards:
-        db_card = setup_card(i)
+    for card in cards:
+        db_card = setup_card(card)
         try:
-            db_cur.execute(
-                'INSERT INTO'
-                ' MAGIC(ID, ASCII, NAME, TYPE, ABILITY, MANA, POWER, TOUGHNESS, LOYALTY,'
-                'LINK) VALUES (:id, :ascii, :name, :type, :ability, :mana, :power, '
-                ':toughness, :loyalty, :link)',
-                db_card)
+            db_cur.execute("""
+                INSERT INTO
+                 MAGIC(ID, ASCII, NAME, TYPE, ABILITY, MANA, POWER, TOUGHNESS, LOYALTY,
+                LINK) VALUES (:id, :ascii, :name, :type, :ability, :mana, :power,
+                :toughness, :loyalty, :link)
+                """, db_card)
             added += 1
         except sqlite3.IntegrityError:
-            print("{name} is a duplicate... skipping.".format(name=db_card['name']))
+            print('{} is a duplicate... skipping.'.format(db_card['name']))
             skipped += 1
 
     db_conn.commit()
     db_conn.close()
 
-    print("\nSuccessfully added: {0} / {1}".format(str(added), str(added + skipped)))
-    print("Successfully skipped: {0} / {1}".format(str(skipped), str(added + skipped)))
+    print('\nSuccessfully added: {} / {}'.format(str(added), str(added + skipped)))
+    print('Successfully skipped: {} / {}'.format(str(skipped), str(added + skipped)))
 
-if len(sys.argv) != 2:
-    print("Excepted a parameter! Please specify the url for parsing.")
 
-process(str(sys.argv[1]))
+def main():
+    arguments = docopt(__doc__, version='Pyser 0.1')
+    process_manabase_resource(arguments['<url>'])
+
+
+if __name__ == '__main__':
+    main()
